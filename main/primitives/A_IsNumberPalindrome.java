dc64514c80c724eca72f272aed539bd7e6f9a5eb
sings18 <srihari.singuru@gmail.com>
2024-03-21 18:51:04 +0530

    changed the package structure

--- main/primitives/A_IsNumberPalindrome.java
+++ main/primitives/A_IsNumberPalindrome.java
@@ -1,4 +1,4 @@
-package main;
+package main.primitives;
 
 public class A_IsNumberPalindrome {
     public static void main(String[] args) {
--- main/primitives/B_ReverseBits.java
+++ main/primitives/B_ReverseBits.java
@@ -1,4 +1,4 @@
-package main;
+package main.primitives;
 
 public class B_ReverseBits {
     public static void main(String[] args) {
